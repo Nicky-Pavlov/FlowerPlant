ed5cc3f78d5671a9b2881940922660200165742d
Nicky-Pavlov <eaa25npa@students.eaaa.dk>
2026-03-02 11:39:13 +0100

    nav bar and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer>
+        
+        <p>Address: 123 Botanical Lane, Green City, 2345 Copenhagen, Denmark</p>
+        <p>Email: flowplant123@flowerplant2026.dk</p>
+        <p>Mobile: +452076765</p>
+      <p>&copy; 2024 FlowerPlant. All rights reserved.</p>
+    </footer>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom';
+import icon from '../assets/Asset 2.svg';
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <Link to="/">Home</Link>&nbsp;&nbsp;
-        <Link to="/my-plants">My Plants</Link>&nbsp;&nbsp;
+    <header className="nav">
+      <Link to="/" className="nav__logo" aria-label="Go to home">
+        <img src={icon} alt="Flower Plant icon" />
+      </Link>
+
+      <nav className="nav__links" aria-label="Main navigation">
+        <Link to="/">Home</Link>
+        <Link to="/my-plants">My Plants</Link>
         <Link to="/about-us">About Us</Link>
       </nav>
     </header>
