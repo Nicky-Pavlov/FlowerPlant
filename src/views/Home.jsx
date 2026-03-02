35d9595f2d5d2061c4f154e403d8e79987c0a81e
Nicky-Pavlov <eaa25npa@students.eaaa.dk>
2026-03-02 12:51:31 +0100

    hero img

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,3 +1,15 @@
+import indoorPlant from '../assets/indoor-plants-studio.jpg';
+import blob from '../assets/blob.svg';
+
+
 export default function Home() {
-  return <h2>Home</h2>;
+  return (
+    <div className="hero">
+      <img src={indoorPlant} alt="Indoor Plant" className="hero-image" />
+      <div className="hero-title-wrap" aria-hidden="true">
+        <img src={blob} alt="" className="hero-blob" />
+        <h1 className="hero-title">FlowerPlant</h1>
+      </div>
+    </div>
+  );
 }
